75bba053d7612fbdd49ab701c0752859c2ecb210
andi <andiks2018@gmail.com>
2024-04-01 23:48:14 +0700

    coba dulu

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
-Route::get('/template', function () {
-    return view('template');
-});
+// Route::get('/template', function () {
+//     return view('template');
+// });
 
 Route::get('/', function () {
     $data = [
@@ -14,12 +14,22 @@ Route::get('/', function () {
     return view('layouts.wrapper', $data);
 });
 
-Route::resource('user', UserController::class);
+// Route::get('/post', function () {
+//     $data = [
+//         'content'=> 'post.index'
+//     ];
+//     return view('layouts.wrapper', $data);
+// });
 
-Route::get('/post', function () {
-    $data = [
-        'content'=> 'post.index'
-    ];
-    return view('layouts.wrapper', $data);
+// pakai route ini
+// Route::resource('user', UserController::class);
+
+// dan pakai route prefix
+Route::prefix('admin')->group(function () {
+
+    Route::resource('/user', UserController::class);
 });
 
+Route::get('greeting', function () {
+    return 'Hello World';
+});
